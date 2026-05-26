08748a0ee7fc93bbc5d141f10154076b6c044478
BT_C09_20 <mobileappdev@bths.edu>
2026-05-26 14:02:34 -0400

    like 3 lines

--- archive_of_downfall/Player.swift
+++ archive_of_downfall/Player.swift
@@ -4,15 +4,8 @@
 //
 //  Created by Student on 5/26/26.
 //
+import Foundation
 
-import SwiftUI
-
-struct Player: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    Player()
+struct Player {
+    
 }
