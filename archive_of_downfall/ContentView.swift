441e2e5d14bb4c5707ed7b87681c31e861d5ce77
BT_C09_20 <mobileappdev@bths.edu>
2026-05-26 13:38:52 -0400

    Initial Commit

---
+++ archive_of_downfall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  archive_of_downfall
+//
+//  Created by Student on 5/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ archive_of_downfall/archive_of_downfallApp.swift
@@ -0,0 +1,17 @@
+//
+//  archive_of_downfallApp.swift
+//  archive_of_downfall
+//
+//  Created by Student on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct archive_of_downfallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
