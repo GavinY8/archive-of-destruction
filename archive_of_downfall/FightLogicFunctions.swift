e36cb617120e8c2c0a777e776b8b0f3c5c05e79d
BT_C09_20 <mobileappdev@bths.edu>
2026-06-01 14:01:54 -0400

    clashing question mark

--- archive_of_downfall/FightLogicFunctions.swift
+++ archive_of_downfall/FightLogicFunctions.swift
@@ -5,3 +5,15 @@
 //  Created by Student on 5/29/26.
 //
 import Foundation
+
+func clash(player: Nugget, enemy: Nugget) {
+    var playerRoll = roll(min: player.deck[0].dice[0].minRoll,max: player.deck[0].dice[0].maxRoll)
+    var enemyRoll = roll(min: enemy.deck[0].dice[0].minRoll,max: enemy.deck[0].dice[0].maxRoll)
+    if () {
+        
+    }
+}
+
+func roll(min: Int, max: Int) -> Int {
+    return Int.random(in: min...max)
+}
