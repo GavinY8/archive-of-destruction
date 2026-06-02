a5d5e5abf3abea3f305a5319066595c79f6d5a7d
BT_C09_20 <mobileappdev@bths.edu>
2026-06-02 13:24:09 -0400

    rolls

--- archive_of_downfall/FightLogicFunctions.swift
+++ archive_of_downfall/FightLogicFunctions.swift
@@ -9,7 +9,7 @@ import Foundation
 func clash(player: Nugget, enemy: Nugget) {
     var playerRoll = roll(min: player.deck[0].dice[0].minRoll,max: player.deck[0].dice[0].maxRoll)
     var enemyRoll = roll(min: enemy.deck[0].dice[0].minRoll,max: enemy.deck[0].dice[0].maxRoll)
-    if () {
+    if (playerRoll > enemyRoll) {
         
     }
 }
