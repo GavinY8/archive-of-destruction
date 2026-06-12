eef016b359c01ea631d84424c039d866c5611971
BT_C09_20 <mobileappdev@bths.edu>
2026-06-12 13:23:29 -0400

    screen transitioning added

---
+++ archive_of_downfall/GameCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  GameCoordinator.swift
+//  archive_of_downfall
+//
+//  Created by Student on 6/12/26.
+//
+
+import SwiftUI
+
+struct GameCoordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameCoordinator()
+}
