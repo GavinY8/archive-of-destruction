6cc5313ba9f0696836d2686316f5fca01d7aedc1
BT_C09_20 <mobileappdev@bths.edu>
2026-06-12 13:28:07 -0400

    wy the hell did it remove my work before

--- archive_of_downfall/ContentView.swift
+++ archive_of_downfall/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VisualNovelTextBoxView()
-        //FightScreen(player: Nugget( name: "player", page: Page(30, 15, [SpeedDice(min: 1, max: 4)], 1.0, 1.5, 2.0, 1.0, 1.5, 2.0), hp: 30, stagger: 15, light: 3, maxLight: 3, deck: CardDatabase.starterDeck, hand: [], discard: [], statuses: []), enemy: Enemies.basic)
+        GameCoordinator()
     }
 }
 
